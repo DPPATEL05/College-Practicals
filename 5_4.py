12112b7a2b50dab63eedd5a9a9839956de783a87
DPPATEL05 <dhairyadppatel071519@gmail.com>
2022-11-28 18:37:59 +0530

    commit

--- 5_4.py
+++ 5_4.py
@@ -2,4 +2,4 @@ a = ord('!')
 b = ord('~')
 
 for i in range(a, b+1):
-    print("Character  {} = {} = {}".format(chr(i), ord(chr(i)), hex(ord(chr(i)))))
+    print("{} = {} = {}".format(chr(i), ord(chr(i)), hex(ord(chr(i)))))
